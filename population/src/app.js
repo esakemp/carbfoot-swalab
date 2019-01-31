cdd961e0c285a8ccff304043c37afa67b7252f69
ikuisma <ilkka.a.kuisma@gmail.com>
2019-01-31 19:30:34 +0200

    Removed updater functionality from population service

--- population/src/app.js
+++ population/src/app.js
@@ -1,10 +1,34 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const { getPopulations, upsertPopulation } = require('./db')
 const { dbconnect } = require('./db')
-const server = require('./server')
+const { PORT } = require('./conf')
+
+const app = express()
+
+app.use(bodyparser.json({ limit: '50mb', extended: true }))
+
+app.get('/populations', async (req, res) => {
+    const populations = await getPopulations()
+    res.json(populations)
+})
+
+app.post('/populations', async (req, res) => {
+    for (let population of req.body) {
+        const { code, ...rest } = population
+        await upsertPopulation({ _id: code, code, ...rest })
+    }
+    res.status(201).send()
+})
+
+app.get('*', async(req, res) => {
+    res.status(404).send()
+})
 
 const start = async () => {
     try {
         await dbconnect()
-        server.start()
+        app.listen(PORT, () => console.log(`Population service listening on port ${PORT}!`))
     } catch (e) {
         throw(e)
     }
--- population/src/conf.js
+++ population/src/conf.js
@@ -1,19 +1,9 @@
-const cron = require('node-cron')
-
 const {
-    CRONSTRING = '0 0 */12 * * *', // Every 12 hours
     PORT = 8000,
-    ZIP_URL = 'http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv',
-    FILE_PREFIX = 'API_SP.POP.TOTL_DS2',
     MONGOURL,
     DBNAME
 } = process.env
 
-const ZIP_PATH = 'assets/tmp/populations.zip'
-const EXTRACT_DIR = 'assets/extracted'
-
-cron.validate(CRONSTRING)
-
 module.exports = {
-    CRONSTRING, PORT, ZIP_URL, FILE_PREFIX, ZIP_PATH, EXTRACT_DIR, MONGOURL, DBNAME
+    PORT, MONGOURL, DBNAME
 }
