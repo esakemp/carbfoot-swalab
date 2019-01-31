23bd7755d508d41e7e344cb69ee6d5cba3d15ce1
ikuisma <ilkka.a.kuisma@gmail.com>
2019-01-31 15:51:24 +0200

    Implemented api fetch + csv parsing from tmp files for co2 and populations

--- updater/src/conf.js
+++ updater/src/conf.js
@@ -1,11 +1,22 @@
 const cron = require('node-cron')
 
 const {
-    CRONSTRING = '*/5 * * * * *' // Every 5 seconds
+    CRONSTRING = '*/30 * * * * *' // Every 5 seconds
 } = process.env
+
+const EXTRACT_DIR = 'assets/extracted'
+const POP_API_URL = 'http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv'
+const POP_CSV_PREFIX = 'API_SP.POP.TOTL_DS2'
+const CO_API_URL = 'http://api.worldbank.org/v2/en/indicator/EN.ATM.CO2E.KT?downloadformat=csv'
+const CO_CSV_PREFIX = 'API_EN.ATM.CO2'
 
 cron.validate(CRONSTRING)
 
 module.exports = {
-    CRONSTRING
+    CRONSTRING,
+    EXTRACT_DIR,
+    POP_API_URL,
+    POP_CSV_PREFIX,
+    CO_API_URL,
+    CO_CSV_PREFIX
 }
--- updater/src/updater.js
+++ updater/src/updater.js
@@ -1,9 +1,29 @@
+const parse = require('csv-parser')
+const { fetchDataStream } = require('./fetcher')
+const { POP_API_URL, POP_CSV_PREFIX, CO_API_URL, CO_CSV_PREFIX } = require('./conf')
+
+const recordsFromCsvStream = (stream) => new Promise((resolve) => {
+    const records = []
+    stream
+        .pipe(parse({ skipLines: 4 }))
+        .on('data', data => {
+            const { 'Country Name': name, 'Country Code': code, ...rest } = data
+            const stats = Object.entries(rest)
+                .filter(([key]) => !isNaN(key))
+                .reduce((acc, [k, v]) => ({...acc, [k]: v}), {})
+            records.push({ code, name, stats })
+        })
+        .on('end', () => resolve(records))
+})
+
 const updatePopulations = async () => {
-    console.log('Population updater not implemented!')
+    const stream = await fetchDataStream(POP_API_URL, POP_CSV_PREFIX)
+    await recordsFromCsvStream(stream)
 }
 
 const updateCo2 = async () => {
-    console.log('CO2 updater not implemented!')
+    const stream = await fetchDataStream(CO_API_URL, CO_CSV_PREFIX)
+    await recordsFromCsvStream(stream)
 }
 
 module.exports = {
