a808b0ad8467331d89afe4c3c9dedfbc6b8db987
ikuisma <ilkka.a.kuisma@gmail.com>
2019-02-11 15:58:20 +0200

    Add mock worldbank api to updater env

--- updater/src/conf.js
+++ updater/src/conf.js
@@ -3,13 +3,13 @@ const cron = require('node-cron')
 const {
     CRONSTRING = '*/30 * * * * *', // Every 5 seconds,
     POPULATION_POST_URL,
-    EMISSION_POST_URL
+    EMISSION_POST_URL,
+    POP_API_URL = 'http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv',
+    CO_API_URL = 'http://api.worldbank.org/v2/en/indicator/EN.ATM.CO2E.KT?downloadformat=csv'
 } = process.env
 
 const EXTRACT_DIR = 'assets/extracted'
-const POP_API_URL = 'http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv'
 const POP_CSV_PREFIX = 'API_SP.POP.TOTL_DS2'
-const CO_API_URL = 'http://api.worldbank.org/v2/en/indicator/EN.ATM.CO2E.KT?downloadformat=csv'
 const CO_CSV_PREFIX = 'API_EN.ATM.CO2'
 
 cron.validate(CRONSTRING)
