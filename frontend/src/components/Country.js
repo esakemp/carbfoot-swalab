840e99362212808c28cd3f390187122bf11f72c4
ikuisma <ilkka.a.kuisma@gmail.com>
2019-02-15 07:13:38 +0200

    Ran standard --fix on all files

--- frontend/src/components/Country.js
+++ frontend/src/components/Country.js
@@ -3,25 +3,24 @@ import { Query } from 'react-apollo'
 import fetchCountry from '../queries/fetchCountry'
 
 const Country = ({ code }) => (
-    <Query query={fetchCountry}
-      variables={{ code }}
-      notifyOnNetworkStatusChange>
-      {({ loading, error, data, networkStatus }) => {
-  
-        if (networkStatus === 4) return "refetching"
-        if (loading) return null
-        if (error) return `error! ${error.message}`
-  
-        return (
-          <div>
-            {data.country.name}, {data.country.code}
-            <div>
-              population was {data.country.stats[0].population} in year {data.country.stats[0].year}
-            </div>
-          </div>
-        )
-      }}
-    </Query>
-  )
+  <Query query={fetchCountry}
+    variables={{ code }}
+    notifyOnNetworkStatusChange>
+    {({ loading, error, data, networkStatus }) => {
+      if (networkStatus === 4) return 'refetching'
+      if (loading) return null
+      if (error) return `error! ${error.message}`
 
-  export default Country
+      return (
+        <div>
+          {data.country.name}, {data.country.code}
+          <div>
+              population was {data.country.stats[0].population} in year {data.country.stats[0].year}
+          </div>
+        </div>
+      )
+    }}
+  </Query>
+)
+
+export default Country
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -4,46 +4,46 @@ import { graphql } from 'react-apollo'
 import fetchAll from '../queries/fetchAll'
 
 const BasicAutocomplete = ({ items, onChange }) => (
-    <Downshift onChange={onChange} itemToString={item => (item ? item.name : '')}>
-        {({
-            getInputProps,
-            getItemProps,
-            isOpen,
-            inputValue,
-            selectedItem,
-            highlightedIndex
-        }) => (
-                <div>
-                    <input {...getInputProps({ placeholder: "search" })} />
-                    {isOpen ? (
-                        <div style={{ border: "1px solid #ccc" }}>
-                            {items.filter(i => !inputValue || i.name
-                                .toLowerCase().includes(inputValue.toLowerCase()))
-                                .slice(0, 10).map((item, index) => (
+  <Downshift onChange={onChange} itemToString={item => (item ? item.name : '')}>
+    {({
+      getInputProps,
+      getItemProps,
+      isOpen,
+      inputValue,
+      selectedItem,
+      highlightedIndex
+    }) => (
+      <div>
+        <input {...getInputProps({ placeholder: 'search' })} />
+        {isOpen ? (
+          <div style={{ border: '1px solid #ccc' }}>
+            {items.filter(i => !inputValue || i.name
+              .toLowerCase().includes(inputValue.toLowerCase()))
+              .slice(0, 10).map((item, index) => (
 
-                                    <div {...getItemProps({ item, index, key: item.code })}
-                                        key={item.code}
-                                        style={{
-                                            backgroundColor:
-                                                highlightedIndex === index ? "gray" : "white",
-                                            fontWeight: selectedItem === item.name ? "bold" : "normal"
-                                        }}
-                                    >
-                                        {item.name}, {item.code}
-                                    </div>
-                                ))}
-                        </div>
-                    ) : null}
+                <div {...getItemProps({ item, index, key: item.code })}
+                  key={item.code}
+                  style={{
+                    backgroundColor:
+                                                highlightedIndex === index ? 'gray' : 'white',
+                    fontWeight: selectedItem === item.name ? 'bold' : 'normal'
+                  }}
+                >
+                  {item.name}, {item.code}
                 </div>
-            )}
-    </Downshift>
+              ))}
+          </div>
+        ) : null}
+      </div>
+    )}
+  </Downshift>
 )
 
-const Search = ({onSelectCountry, data: { allCountries = [] }}) => (
-    <div>
-        <BasicAutocomplete items={allCountries}
-            onChange={selectedItem => onSelectCountry(selectedItem.code)} />
-    </div>
+const Search = ({ onSelectCountry, data: { allCountries = [] } }) => (
+  <div>
+    <BasicAutocomplete items={allCountries}
+      onChange={selectedItem => onSelectCountry(selectedItem.code)} />
+  </div>
 )
 
 export default graphql(fetchAll)(Search)
