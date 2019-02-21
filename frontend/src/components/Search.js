6233777c69cf9b1c7362d82bf0a937b425167ec6
esakemp <esa.kemppainen@helsinki.fi>
2019-02-21 08:56:52 +0200

    frontend search bar almost works as wanted

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -4,123 +4,116 @@ import Downshift from 'downshift'
 import { graphql } from 'react-apollo'
 import fetchAll from '../queries/fetchAll'
 
-import { withStyles } from '@material-ui/core/styles'
-import TextField from '@material-ui/core/TextField'
-import Paper from '@material-ui/core/Paper'
-import MenuItem from '@material-ui/core/MenuItem'
-import Chip from '@material-ui/core/Chip'
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import Chip from '@material-ui/core/Chip';
 
-const suggestions = [
-  { label: 'Finland' },
-  { label: 'Fiji' },
-  { label: 'Testimaa' },
-  { label: 'Testimaa2' },
-  { label: 'Testimaa3' },
-  { label: 'Testimaa4' },
-  { label: 'Testimaa5' },
-]
-
+var suggestions = []
 
 function renderInput(inputProps) {
-  const { inputprops, ref, ...other } = inputProps
+  const { InputProps, ref, ...other } = inputProps;
 
   return (
     <TextField
-      inputprops={{
+      InputProps={{
         inputRef: ref,
-        ...inputprops,
+        ...InputProps,
       }}
       {...other}
     />
-  )
+  );
 }
 
 function renderSuggestion({ suggestion, index, itemProps, highlightedIndex, selectedItem }) {
-  const isHighlighted = highlightedIndex === index
-  const isSelected = (selectedItem || '').indexOf(suggestion.label) > - 1
+  const isHighlighted = highlightedIndex === index;
+  const isSelected = (selectedItem || '').indexOf(suggestion.name) > -1;
 
   return (
     <MenuItem
       {...itemProps}
-      key={suggestion.label}
+      key={suggestion.code}
       selected={isHighlighted}
       component="div"
       style={{
         fontWeight: isSelected ? 500 : 400,
       }}
     >
-      {suggestion.label}
+      {suggestion.name}
     </MenuItem>
-  )
+  );
 }
 
-function getSuggestions(value) {
-  const inputValue = deburr(value.trim()).toLowerCase()
-  const inputLength = inputValue.length
-  let count = 0
 
-  console.log(inputValue)
+function getSuggestions(value) {
+  const inputValue = deburr(value.trim()).toLowerCase();
+  const inputLength = inputValue.length;
+  let count = 0;
 
   return inputLength === 0
     ? []
     : suggestions.filter(suggestion => {
       const keep =
-        count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue
+        count < 5 && suggestion.name.slice(0, inputLength).toLowerCase() === inputValue;
 
       if (keep) {
-        count += 1
+        count += 1;
       }
 
-      return keep
-    })
+      return keep;
+    });
 }
 
-
-class SearchBar extends Component {
+class DownshiftMultiple extends React.Component {
   state = {
     inputValue: '',
-    selectedItem: []
-  }
+    selectedItem: [],
+  };
 
   handleKeyDown = event => {
-    const { inputValue, selectedItem } = this.state
+    const { inputValue, selectedItem } = this.state;
     if (selectedItem.length && !inputValue.length && event.key === 'Backspace') {
       this.setState({
-        selectedItem: selectedItem.slice(0, selectedItem.length - 1)
-      })
+        selectedItem: selectedItem.slice(0, selectedItem.length - 1),
+      });
     }
-  }
+  };
+
   handleInputChange = event => {
-    this.setState({ inputValue: event.target.value })
-  }
+    this.setState({ inputValue: event.target.value });
+  };
 
-  handleChange = (item) => {
-    let { selectedItem } = this.state
+  handleChange = item => {
+    let { selectedItem } = this.state;
+
     if (selectedItem.indexOf(item) === -1) {
-      selectedItem = { ...selectedItem, item }
+      selectedItem = [...selectedItem, item];
     }
-
+    console.log(selectedItem)
     this.setState({
       inputValue: '',
-      selectedItem
-    })
-  }
-  handleDelete = (item) => {
+      selectedItem,
+    });
+  };
+
+  handleDelete = item => () => {
     this.setState(state => {
-      const selectedItem = [...state.selectedItem]
-      selectedItem.splice(selectedItem.indexOf(item), 1)
-      return { selectedItem }
-    })
-  }
+      const selectedItem = [...state.selectedItem];
+      selectedItem.splice(selectedItem.indexOf(item), 1);
+      return { selectedItem };
+    });
+  };
 
   render() {
-    const { inputValue, selectedItem } = this.state
+    const { inputValue, selectedItem } = this.state;
 
     return (
       <Downshift
+        id="downshift-multiple"
         inputValue={inputValue}
         onChange={this.handleChange}
         selectedItem={selectedItem}
+        itemToString={item => (item ? item.code : '')}
       >
         {({
           getInputProps,
@@ -144,27 +137,27 @@ class SearchBar extends Component {
                   )),
                   onChange: this.handleInputChange,
                   onKeyDown: this.handleKeyDown,
-                  placeholder: 'select multiple countries',
+                  placeholder: 'Select multiple countries',
                 }),
                 label: 'Label',
               })}
               {isOpen ? (
                 <Paper square>
-                  {getSuggestions(inputValue2).map((suggestion, index) => {
+                  {getSuggestions(inputValue2).map((suggestion, index) =>
                     renderSuggestion({
                       suggestion,
                       index,
-                      itemProps: getItemProps({ item: suggestion.label }),
+                      itemProps: getItemProps({ item: suggestion.code, index, key: suggestion.code }),
                       highlightedIndex,
                       selectedItem: selectedItem2,
-                    })
-                  })}
+                    }),
+                  )}
                 </Paper>
               ) : null}
             </div>
           )}
       </Downshift>
-    )
+    );
   }
 }
 
@@ -205,14 +198,14 @@ const BasicAutocomplete = ({ items, onChange }) => (
 function Search({ onSelectCountry, data: { allCountries = [] } }) {
   //<BasicAutocomplete items={allCountries}
   //onChange={selectedItem => onSelectCountry(selectedItem.code)} />
-
+  suggestions = allCountries
   return (
     <div>
       <BasicAutocomplete items={allCountries}
         onChange={selectedItem => onSelectCountry(selectedItem.code)} />
-        <SearchBar/>
+      <DownshiftMultiple />
     </div>
-    
+
   )
 }
 
