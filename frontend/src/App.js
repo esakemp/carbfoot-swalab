48e0d296bb590e5060bd2a27cd0e4f88264f719c
Ilkka Kuisma <ilkka.a.kuisma@gmail.com>
2019-02-22 07:24:44 +0200

    Wider breakpoint for larger screens.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,46 @@
-import React, { Component } from 'react'
-import withStyles from '@material-ui/core/styles/withStyles'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
-import Search from './components/Search'
-import Country from './components/Country'
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Search from "./components/Search";
+import Country from "./components/Country";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql'
-})
+  uri: "http://localhost:8000/graphql"
+});
 
 const styles = theme => ({
   main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto'
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto"
     }
   },
   paper: {
     marginTop: theme.spacing.unit * 8,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
     padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
       .spacing.unit * 3}px`
   }
-})
+});
 
 class App extends Component {
-  state = { selectedCountry: null }
+  state = { selectedCountry: null };
 
   onSelectCountry = value => {
-    this.setState({ selectedCountry: value })
-    console.log(value)
-  }
+    this.setState({ selectedCountry: value });
+    console.log(value);
+  };
 
   render() {
-    const { classes } = this.props
+    const { classes } = this.props;
 
     return (
       <div className={classes.main}>
@@ -54,8 +54,8 @@ class App extends Component {
           </div>
         </ApolloProvider>
       </div>
-    )
+    );
   }
 }
 
-export default withStyles(styles)(App)
+export default withStyles(styles)(App);
