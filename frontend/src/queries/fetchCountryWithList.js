2a3593c75eb1574b81642a66cba5ddc9f6927a7c
Ilkka Kuisma <ilkka.a.kuisma@gmail.com>
2019-02-28 11:18:08 +0200

    Fix to new endpoint

--- frontend/src/queries/fetchCountryWithList.js
+++ frontend/src/queries/fetchCountryWithList.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 
 export default gql`
-  query findCountries($codes: [String!]) {
+  query countries($codes: [String!]) {
     countries(codes: $codes) {
       name
       code
