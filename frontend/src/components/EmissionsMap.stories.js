7cd7c61e73926ee867bf8e4f1da85fac5bd0a02f
Ilkka Kuisma <ilkka.a.kuisma@gmail.com>
2019-03-04 22:26:12 +0200

    Updated emissions map storybook

--- frontend/src/components/EmissionsMap.stories.js
+++ frontend/src/components/EmissionsMap.stories.js
@@ -2,5 +2,10 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import EmissionsMap from './EmissionsMap'
 
+const data = [
+    { code: 'FIN', value: 100.425 },
+    { code: 'SWE', value: 200.434 }
+]
+
 storiesOf('EmissionsMap', module)
-  .add('default', () => <EmissionsMap code="FIN" />)
+  .add('default', () => <EmissionsMap data={data} />)
